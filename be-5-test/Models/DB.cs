c8969174e92b3619723d3a921c5823314727bfa7
Abel Sassu <relinquished@hotmail.it>
2024-02-29 18:42:45 +0100

    Aggiungere i file di progetto.

---
+++ be-5-test/Models/DB.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace be_5_test.Models
+{
+    public class DB
+    {
+        public static string connectionString = "server=DESKTOP-O9F15V0\\SQLEXPRESS; Initial Catalog=PoliziaMunicipale; Integrated Security=true; TrustServerCertificate=true";
+        public static SqlConnection conn = new SqlConnection(connectionString);
+    }
+}
+
